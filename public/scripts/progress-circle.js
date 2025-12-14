bc8227100fad17219d48538464068bba388f30e0
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-12-14 20:10:22 +0100

    feat: create circle progress bar

---
+++ public/scripts/progress-circle.js
@@ -0,0 +1,33 @@
+const circularProgress = document.querySelectorAll(".circular-progress");
+
+
+Array.from(circularProgress).forEach((progressBar) => {
+
+  const progressValue = progressBar.querySelector(".percentage");
+  const innerCircle = progressBar.querySelector(".inner-circle");
+  const progressNumber = progressBar.querySelector(".progress-number")
+
+  
+  let startValue = 0,
+    endValue = Number(progressBar.getAttribute("data-percentage")),
+    speed = 50,
+    secColor = "#4A90E2",
+    accColor = "#2ECC71"
+
+  const progress = setInterval(() => {
+    startValue++;
+    progressNumber.textContent = `${startValue}`;
+   // progressValue.style.color = `${secColor}`;
+
+    innerCircle.style.backgroundColor = 'white';
+
+    progressBar.style.background = `conic-gradient(${accColor} ${
+      startValue * 3.6}deg, rgba(0,0,0,0.3) 0deg)`;
+
+    if (startValue === endValue) {
+        console.log("finish")
+      clearInterval(progress);
+    }
+
+  }, speed);
+});
